4e6d8b6afbd3dc98053fa167d8a619e43b33348d
Jjie0707 <1778836683@qq.com>
2024-12-18 20:58:46 +0800

    Jjie
    Jjie

---
+++ test.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define MASK 2
+// int main()
+// {
+//     printf("%d ",sizeof(MASK));
+//     return 0;
+// }
+
+// int main()
+// {
+//     int i=1;
+//     i=~i;
+//     printf("%d ",i);
+//     return 0;
+// }
+
+
+// //栈的实现
+// int main()
+// {
+//     char *p="abcdefg";
+//     printf("%p\n",p);
+//     printf("%p\n","abcdefg");
+//     printf("%s","abcdefg");
+//     printf("%s",p);
+//     return 0;
+// }
+
+// struct St{
+    
+//     short a:4;
+//     int b:7;
+//     int c:3;
+//     char d:1;
+// };
+
+// int main()
+// {
+//     printf("%d ",sizeof(struct St));
+//     return 0;
+// }
+
